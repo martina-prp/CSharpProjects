523329c08e4c07890fe6d4036450f6257f93f38e
Student <Student>
2016-11-04 17:07:10 +0200

    Committing Complex Project.

---
+++ Complex/Complex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Complex
+    {
+        public int Real { get; set; }
+        public int Img { get; set; }
+
+        public Complex(int real, int img)
+        {
+            this.Real = real;
+            this.Img = img;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} + {1}i", this.Real, this.Img);
+        }
+
+        public static Complex operator +(Complex comp1, Complex comp2)
+        {
+            return (new Complex((comp1.Real + comp2.Real), (comp1.Img + comp2.Img)));
+        }
+
+        public static Complex operator -(Complex comp1, Complex comp2)
+        {
+            return (new Complex((comp1.Real - comp2.Real), (comp1.Img - comp2.Img)));
+        }
+    }
+}
---
+++ Complex/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int real, img;
+            // It is an array of two complex numbers for the puprose of the task.
+            Complex[] complexArray = new Complex[2];
+            Console.WriteLine("Enter two complex numbers!");
+            for (int i=0; i<2; i++)
+            {
+                Console.WriteLine("Enter complex number by using space!");
+                string[] line = Console.ReadLine().Split(' ');
+                while (!int.TryParse(line[0], out real) || !int.TryParse(line[1], out img))
+                {
+                    Console.WriteLine("Enter valid values!");
+                    line = Console.ReadLine().Split(' ');
+                }
+                complexArray[i] = new Complex(real, img);
+            }
+            Console.WriteLine("Enter operation (add or sub):");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "add":
+                    Console.WriteLine("Result of addition: {0}", (complexArray[0] + complexArray[1]));
+                    break;
+                case "sub":
+                    Console.WriteLine("Result of substraction: {0}", (complexArray[0] - complexArray[1]));
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation!");
+                    break;
+            }
+        }
+    }
+}
